8c5864a7b5345fa736d9f7c57b87832cbdb8167e
setareh <setarehsaboory@gmail.com>
2025-10-22 04:43:46 +0200

    rotate on mobile version

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-
 import Experience from './Components/Experience'
 import Interface from './Interface/Interface'
-import LevaControls from './Interface/Leva'
+import RotateOverlay from './Interface/RotateOverlay'
 import { useStore } from './Store/useStore'
 import WeathersAudio from './Utils/Audio/WeathersAudio'
+import useBreakpoint from './Utils/useBreakpoint'
+import useOrientation from './Utils/useOrientation'
 
 
 
 function App() {
   const {Weather} =useStore()
+  const breakpoint=useBreakpoint()
+  const orientation = useOrientation()
+  const rotateIsNeeded = breakpoint==='mobile' && orientation==='portrait';
 
   return (
     <>
+    <RotateOverlay show={rotateIsNeeded} />
     <Experience/>
     <Interface/>
     <WeathersAudio src={`./Audios/${Weather}.mp3`} />
-
-    {/* <LevaControls/> */}
     </>
   )
 }
