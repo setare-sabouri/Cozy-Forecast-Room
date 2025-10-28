4df53523d545dc15195f844a24d2bb19b2a4972f
setareh <setarehsaboory@gmail.com>
2025-10-28 04:23:23 +0100

    pl

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -9,6 +9,8 @@ import * as THREE from 'three'
 import useBreakpoint from '../Utils/useBreakpoint'
 import { useStore } from '../Store/useStore'
 import Lights from './Lights/Lights'
+import { Suspense } from 'react'
+import PlaceHolder from '../Utils/PlaceHolder/PlaceHolder'
 
 
 
@@ -19,7 +21,9 @@ const Experience = () => {
   return (
     <Canvas  camera={{ position: breakpoint === 'desktop' ? [-0.5, 7.4, 34] : [-0.5, 8, 37], fov: 30, rotation: [-Math.PI / 15, 0, 0] }}>
       <PresentationControls polar={[-Math.PI / 3, Math.PI / 3]} azimuth={[-Math.PI / 2, Math.PI / 2]} snap speed={0.5}>
-        <WindowMDL />
+       <Suspense fallback={<PlaceHolder/>}>
+         <WindowMDL />
+       </Suspense>
         {Weather !== 'Sunny' && <VolumetricEffect color={new THREE.Color(0xf5f5f5)} texturePath="/Textures/fire.png" position={[8.8, -0.6, 2]} scale={[2, 2, 2]} magnitude={2} speed={1} opacity={0.5} />}
         <VolumetricEffect color={new THREE.Color('#969292')} texturePath="/Textures/steam.jpg" position={[-2.4, 1.8, 1.7]} scale={[1, 4, 1]} magnitude={4} speed={0.2} opacity={0.15} />
       </PresentationControls>
