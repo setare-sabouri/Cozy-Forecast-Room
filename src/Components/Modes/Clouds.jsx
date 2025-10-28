adfb471fd66d58fec582c615b9ecb724c104f92d
setareh <setarehsaboory@gmail.com>
2025-10-28 05:14:21 +0100

    snow man

--- src/Components/Modes/Clouds.jsx
+++ src/Components/Modes/Clouds.jsx
@@ -11,7 +11,7 @@ const SeasonalClouds = ({ position = [10, 9, 0], bounds = [1, 1, 10], color = "#
             color={color}
             fade={50}
             speed={Weather === 'Stormy' ? 0.8 : Weather === 'Rainy' ? 0.2 : Weather === 'Cloudy' ? 0.3 : Weather === 'Snowy' ? 0.1 : 5}
-            growth={Weather === 'Stormy' ? 8 : Weather === 'Rainy' ? 6 : Weather === 'Cloudy' ? 1 : Weather === 'Snowy' ? 0.1 : 5}
+            growth={Weather === 'Stormy' ? 8 : Weather === 'Rainy' ? 6 : Weather === 'Cloudy' ? 1 : Weather === 'Snowy' ? 1 : 5}
             segments={40}
             volume={Weather === 'Stormy' ? 10 : Weather === 'Rainy' ? 0 : Weather === 'Cloudy' ? 18 : Weather === 'Snowy' ? 30 : 5}
         />
--- src/Components/Modes/Snowy/SnowyMaterial.jsx
+++ src/Components/Modes/Snowy/SnowyMaterial.jsx
@@ -3,18 +3,23 @@ import Particles from '../Particles'
 
 import SeasonalClouds from '../Clouds'
 import { useStore } from '../../../Store/useStore'
+import Lights from '../../Lights/Lights'
 
 const SnowyMaterial = () => {
   const { Weather } = useStore()
-  // const snowLand = useGLTF('/Models/snowroad.glb')
+  const snow = useGLTF('/Models/snow_man.glb')
+
 
   return (
     <MeshPortalMaterial>
       <ambientLight intensity={0.5} />
-      <Environment preset="city" />
-      <color attach="background" args={['#5a5f60']} />
+      <directionalLight  position={[0, 10, 0]}  intensity={2} />
+      <color attach="background" args={['#4d5151']} />
       <Particles count={500} mode={Weather} distanceToWindow={-0.5} />
-      <SeasonalClouds position={[50, 14, 0]} bounds={[1, 1, 40]} color='#ffffff' />
+      <SeasonalClouds position={[50, 14, 0]} bounds={[1, 10, 20]} color='#ffffff' />
+      <group scale={1} position={[30, -24, 25]} rotation={[0, -Math.PI / 3, 0]} >
+        <primitive object={snow.scene} />
+      </group>
     </MeshPortalMaterial>
   )
 }
